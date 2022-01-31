b4642443fd05bc5a80c03be42449c2396a0be344
MichaelHWatkins <58484423+MichaelHWatkins@users.noreply.github.com>
2022-01-31 12:31:22 -0800

    Delete EnvironmentVariables.cs
    removing sensitive info

--- VisualData/Models/EnvironmentVariables.cs
+++
@@ -1,8 +0,0 @@
-namespace VisualData.Models
-{
-    public static class EnvironmentVariables
-    {
-        public static string ApiKey = "d5a5ffc5bc3342c58cd1e865600ed7de";
-        public static string ApiNews = "323e37cdd50245f095c69733e6e1c869";
-    }
-}
